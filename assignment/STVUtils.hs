42068b1903692cf7a1be0550d7816a138b8832ee
Oleksandr Kononov <sashakon13@gmail.com>
2019-05-01 13:32:32 +0100

    Comment

--- assignment/STVUtils.hs
+++ assignment/STVUtils.hs
@@ -1,17 +1,28 @@
+-- Student Name: Oleksandr Kononov
+-- Student Number: 20071032
+-- Student Course: Entertainment Systems
+
+-- Utility functions for the single transferable voting class
+
 module STVUtils where
 
+-- Given a file of votes (list of lists) read it and return the list of lists structure
 getBallots :: String -> IO [[String]]
 getBallots file = do ballots <- readFile file
                      readIO ballots
 
+-- Given the votes and n number of candidates to elect, calculate the necessary quota
 getQuota :: [[a]] -> Int -> Int
 getQuota vss n = (length vss `div` (n + 1)) + 1
 
+-- Given a list, count the number of occurance of the given element
 count :: Eq a => a -> [a] -> Int
 count x = length . filter (x==)
 
+-- Eliminate a given element from a given list of lists
 elim :: Eq a => a -> [[a]] -> [[a]]
 elim x = map $ filter (/=x)
 
+-- Get the first elements from a list of lists
 firsts :: [[a]] -> [a]
 firsts = map head
--- assignment/stv_voting.hs
+++ assignment/stv_voting.hs
@@ -1,16 +1,27 @@
+-- Student Name: Oleksandr Kononov
+-- Student Number: 20071032
+-- Student Course: Entertainment Systems
+
+-- The main single transferable vote file to load.
+-- Call the function "stv" for voting
+
 import STVUtils
 import Debug.Trace
 import Data.List
 import Data.Ord (comparing)
 
+-- Ranks votes based on their weight (frequency of occurance)
+-- Note: nub is a nice little function that finds only unique elements of a list
 rank :: Ord a => [a] -> [(a,Int)]
 rank vs = sortBy moreVotes [(v, count v vs) | v <- nub vs]
           where
             moreVotes (_, vc1) (_, vc2) = compare vc2 vc1
 
+-- Find and returns all the votes that belong to a given candidate
 votesOf :: Eq a => [[a]] -> a -> [[a]]
 votesOf votes a = [vote | vote <- votes, head vote == a]
 
+-- When candidate is elected, redo the votes and weights of votes based on surplus from quota
 redoVotes :: (Ord a, Show a) => [[a]] -> (a,Int) -> Int -> [[a]]
 redoVotes votes winner q | surplus > 0 = take surplus winnerlessVotes ++ restOfVotes
                          | otherwise = elim (fst winner) votes
@@ -34,6 +45,7 @@ winners n c q votes elected | length rankedVotes == 1 = reverse newElected
                               elimVotes = elim (fst $ last rankedVotes) votes
                               newElected = (fst (head rankedVotes), c) : elected
 
+-- Call this function to start calculating the votes
 stv :: Int -> FilePath -> IO ()
 stv n ballotsFile = do ballots <- getBallots ballotsFile
                        let quota' = getQuota ballots n
